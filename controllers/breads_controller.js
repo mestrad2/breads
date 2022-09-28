5d8851cfd3edbf91ad998962807c7e1d37eae1f8
Manuel Estrada <manuel.estrada16@gmail.com>
2022-09-28 18:50:32 -0400

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -22,11 +22,19 @@ breads.get('/new', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread: Bread[req.params.arrayIndex]
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
     })
   } else {
-    res.render('error')
+    res.render('404')
   }
+})
+
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
 })
 
 // CREATE
