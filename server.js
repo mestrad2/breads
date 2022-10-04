1c6ceef90f3e4124d5d53f7cddbc91ee10252377
Manuel Estrada <manuel.estrada16@gmail.com>
2022-10-03 20:45:50 -0400

    added mongoose

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // DEPENDENCIES 
 const express = require('express')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+
 
 // CONFIGURATION
 require('dotenv').config()
@@ -28,6 +30,11 @@ app.use('/breads', breadsController)
 app.get('*', (req, res) => {
   res.send('404')
 })
+
+//MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 
 // LISTEN
 app.listen(PORT, () => {
