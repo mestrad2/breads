3f6a969e7529432e8ecfedb5aab2435c73e76f10
Manuel Estrada <manuel.estrada16@gmail.com>
2022-10-15 09:56:49 -0400

    updated controllers

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -2,13 +2,15 @@ const express = require('express')
 const baker = express.Router()
 const Baker = require('../models/baker')
 const bakerSeedData = require('../models/baker_seed')
-const { populate } = require('../models/bread')
 
+// SEED DATA
 baker.get('/data/seed', (req, res) => {
     Baker.insertMany(bakerSeedData)
         .then(res.redirect('/breads'))
 })
 
+
+// INDEX
 baker.get('/', (req, res) => {
     Baker.find()
         .populate('breads')
@@ -17,13 +19,26 @@ baker.get('/', (req, res) => {
         })
 })
 
+// SHOW
 baker.get('/:id', (req,res) => {
     Baker.findById(req.params.id)
-    .populate('breads')
+    .populate({
+        path: 'breads',
+        options: {limit: 2}
+    })
     .then(foundBaker => {
         res.render('bakerShow', {
             baker: foundBaker
         })
+    })
+})
+
+
+// DELETE
+baker.delete('/:id', (req, res) => {
+    Baker.findByIdAndDelete(req.params.id)
+    .then(deletedBaker => {
+        res.status(303).redirect('/breads')
     })
 })
 
--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,50 +1,79 @@
 const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
+const Baker = require('../models/baker.js')
 
 // INDEX
-breads.get('/', (req, res) => {
-  Bread.find()
-      .then(foundBreads => {
-          res.render('index', {
-              breads: foundBreads,
-              title: 'Index Page'
-          })
-      })
+breads.get('/', async (req, res) => {
+  const foundBakers = await Baker.find().lean()
+  const foundBreads = await Bread.find().limit(2).lean()
+  console.log(foundBreads)
+  res.render('index', {
+    breads: foundBreads,
+    bakers: foundBakers,
+    title: 'Index Page'
+  })
 })
 
 
 
 // NEW
 breads.get('/new', (req, res) => {
-  res.render('new')
+  Baker.find()
+    .then(foundBakers => {
+      res.render('new', {
+        bakers: foundBakers
+      })
+    })
 })
 
 // EDIT
-breads.get('/:indexArray/edit', (req, res) => {
-  res.render('edit', {
-    bread: Bread[req.params.indexArray],
-    index: req.params.indexArray
+breads.get('/:id/edit', (req, res) => {
+  Baker.find().then(foundBakers => {
+    Bread.findById(req.params.id)
+      .then(foundBread => {
+        res.render('edit', {
+          bread: foundBread,
+          bakers: foundBakers
+        })
+      })
   })
 })
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex],
-      index: req.params.arrayIndex,
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+    .populate('baker')
+    .then(foundBread => {
+      const bakedBy = foundBread.getBakedBy()
+      // console.log(bakedBy)
+      res.render('show', {
+        bread: foundBread
+      })
     })
-  } else {
-    res.render('error')
-  }
 })
 
 
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
-  Bread.splice(req.params.indexArray, 1)
-  res.status(303).redirect('/breads')
+breads.delete('/:id', (req, res) => {
+  Bread.findByIdAndDelete(req.params.id)
+    .then(deletedBread => {
+      res.status(303).redirect('/breads')
+    })
+})
+
+// UPDATE
+breads.put('/:id', (req, res) => {
+  if (req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then(updatedBread => {
+      console.log(updatedBread)
+      res.redirect(`/breads/${req.params.id}`)
+    })
 })
 
 // CREATE
@@ -59,19 +88,6 @@ breads.post('/', (req, res) => {
   }
   Bread.create(req.body)
   res.redirect('/breads')
-})
-
-
-
-// UPDATE
-breads.put('/:arrayIndex', (req, res) => {
-  if(req.body.hasGluten === 'on'){
-    req.body.hasGluten = true
-  } else {
-    req.body.hasGluten = false
-  }
-  Bread[req.params.arrayIndex] = req.body
-  res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 
