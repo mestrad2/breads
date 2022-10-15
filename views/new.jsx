b287a86dea56e10931a445dc7e2357b4a0c7f750
Manuel Estrada <manuel.estrada16@gmail.com>
2022-10-15 10:06:08 -0400

    removed old comments

--- views/new.jsx
+++ views/new.jsx
@@ -28,12 +28,7 @@ function New({bakers}) {
               <option value={baker.id} key={baker.id}>{baker.name}</option>
             )
           })}
-          {/* <option value="Rachel">Rachel</option>
-          <option value="Monica">Monica</option>
-          <option value="Joey">Joey</option>
-          <option value="Chandler">Chandler</option>
-          <option value="Ross">Ross</option>
-          <option value="Phoebe">Phoebe</option> */}
+
         </select>
         <label htmlFor="hasGluten">Has Gluten?</label>
         <input
